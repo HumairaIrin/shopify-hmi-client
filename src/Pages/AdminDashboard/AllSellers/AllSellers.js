1c8f7404ecf4893f158b7ca22c9e83c4af7bdc11
HumairaIrin <humairamuntaha24@gmail.com>
2022-11-30 22:35:38 +0600

    all sellers component added

--- src/Pages/AdminDashboard/AllSellers/AllSellers.js
+++ src/Pages/AdminDashboard/AllSellers/AllSellers.js
@@ -1,19 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
-const AllBuyers = () => {
-    const type = 'buyer';
-    const { data: buyers = [] } = useQuery({
-        queryKey: ['users', 'buyers', type],
+const AllSellers = () => {
+    const type = 'seller';
+    const { data: sellers = [] } = useQuery({
+        queryKey: ['users', 'sellers', type],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/users/buyers?type=${type}`)
+            const res = await fetch(`http://localhost:5000/users/sellers?type=${type}`)
             const data = await res.json();
             return data;
         }
     })
     return (
         <div className="overflow-x-auto w-[90%] mx-auto md:w-4/5 my-5">
-            <h3 className="text-2xl font-bold mb-3 text-center">All Buyers</h3>
+            <h3 className="text-2xl font-bold mb-3 text-center">All Sellers</h3>
             <table className="table table-zebra w-full">
                 <thead>
                     <tr>
@@ -25,11 +25,11 @@ const AllBuyers = () => {
                 </thead>
                 <tbody>
                     {
-                        buyers.map((buyer, i) =>
+                        sellers.map((seller, i) =>
                             <tr key={i}>
                                 <th>{i + 1}</th>
-                                <td>{buyer.name}</td>
-                                <td>{buyer.email}</td>
+                                <td>{seller.name}</td>
+                                <td>{seller.email}</td>
                                 <td><button className='btn btn-outline btn-sm btn-secondary'>Delete</button></td>
                             </tr>)
                     }
@@ -40,4 +40,4 @@ const AllBuyers = () => {
     );
 };
 
-export default AllBuyers;
+export default AllSellers;
